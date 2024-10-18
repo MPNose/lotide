7c76b721eeaff6db5d36a6808b316b3b486e6b6e
MPNose <mpnoseworthy89@gmail.com>
2024-10-17 19:28:31 -0700

    created a take until function

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      return result;
+    } else {
+      result.push(array[i]);
+    }
+  }
+return result;
+};
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+//console.log(results1);
+
+//console.log('---');
+
+//const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+//const results2 = takeUntil(data2, x => x === ',');
+//console.log(results2);
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++){
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true
+};
+const assertArraysEqual = function(array1, array2) {
+  let check = eqArrays(array1, array2);
+    if (check === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`)
+  }
+};
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2])
