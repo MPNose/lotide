f6f467fa5ac396a5ebdb3773c5dcf8809718e47e
MPNose <mpnoseworthy89@gmail.com>
2024-10-21 13:42:51 -0700

    refactored tail, added test parameters

--- tail.js
+++ tail.js
@@ -1,13 +1,18 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
+
+
+
+
 const tail = function(arr) {
-  return arr.slice(1);
+   let result = arr.slice(1);
+   
+   return result;
 };
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+
+
+
+
+
+
+
+module.exports = tail;
