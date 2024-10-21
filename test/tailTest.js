4b27d6c4696033bdde0703344ad203735b17dd75
MPNose <mpnoseworthy89@gmail.com>
2024-10-21 13:48:34 -0700

    added test to determine unchanged array

--- test/tailTest.js
+++ test/tailTest.js
@@ -9,3 +9,6 @@ const result = tail(['yo yo', "lighthouse", "labs"]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "lighthouse");
 assertEqual(result[1], "labs");
+const words = ['yo yo', "lighthouse", "labs"];
+tail(words);
+assertEqual(words.length, 3)
