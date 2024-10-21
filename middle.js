35865b164d83013e142a704ff85032121b47f790
MPNose <mpnoseworthy89@gmail.com>
2024-10-21 16:20:29 -0700

    refactored middle to just function, created test function

--- middle.js
+++ middle.js
@@ -1,22 +1,9 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true
-};
-const assertArraysEqual = function (array1, array2) {
-  let check = eqArrays(array1, array2);
-  if (check === true) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`)
-  }
-};
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
+
+
+
+
 const middle = function (array) {
   if (array.length < 3) {
     return [];
@@ -30,4 +17,8 @@ const middle = function (array) {
   return [array[half - 1], array[half]]
 };
 
-assertArraysEqual(middle([1, 2, 3, 5, 6, 7]), [3, 5]);
+
+
+module.exports = middle;
+
+
