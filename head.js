80b3900556c0603449cacef88fec47ae578c4c58
MPNose <mpnoseworthy89@gmail.com>
2024-10-21 12:55:16 -0700

    refactored and created test file

--- head.js
+++ head.js
@@ -1,12 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
+
+
+
 const head = function(arr) {
   return arr[0];
 }
-assertEqual(head([5]), 5);
-assertEqual(head(["Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
