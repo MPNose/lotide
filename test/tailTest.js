6b67c588fee72deba35aea60f3a78a6173d93f45
MPNose <mpnoseworthy89@gmail.com>
2024-10-21 17:11:18 -0700

    refactored tail with mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,33 @@
+const assert = require('chai').assert;
 const tail = require("../tail");
 const assertEqual = require("../assertEqual");
 
+describe('#tail', () => {
+  
+  it("returns a new array with a shorter length", () => {
+    const result = tail(['yo yo', "lighthouse", "labs"]);
+    assert.strictEqual(result.length, 2);
+  });
+
+  it("returns a new first element in an array", () => {
+    const result = tail(['yo yo', "lighthouse", "labs"]);
+    assert.strictEqual(result[0], "lighthouse");
+  });
+
+  it("returns a new second array index", () => {
+    const result = tail(['yo yo', "lighthouse", "labs"]);
+    assert.strictEqual(result[1], "labs");
+  });
+  
+  it("doesn't alter the original array", () => {
+    const words = ['yo yo', "lighthouse", "labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+
+});
 
 
 
 
-const result = tail(['yo yo', "lighthouse", "labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "lighthouse");
-assertEqual(result[1], "labs");
-const words = ['yo yo', "lighthouse", "labs"];
-tail(words);
-assertEqual(words.length, 3)
+
